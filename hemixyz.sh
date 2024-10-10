59cbdbcb6b4d74bdb2cbb046eb9f737c93e803f1
RB <153273247+ibuyshite@users.noreply.github.com>
2024-10-10 11:10:17 +0530

    Update hemixyz.sh

--- hemixyz.sh
+++ hemixyz.sh
@@ -35,13 +35,13 @@ fi
 
 if [ "$ARCH" == "x86_64" ]; then
     show "Downloading for x86_64 architecture..."
-    wget --quiet --show-progress https://github.com/hemilabs/heminetwork/releases/download/v0.4.3/heminetwork_v0.4.4_linux_amd64.tar.gz -O heminetwork_v0.4.3_linux_amd64.tar.gz
-    tar -xzf heminetwork_v0.4.3_linux_amd64.tar.gz > /dev/null
+    wget --quiet --show-progress https://github.com/hemilabs/heminetwork/releases/download/v0.4.4/heminetwork_v0.4.4_linux_arm64.tar.gz -O heminetwork_v0.4.3_linux_arm64.tar.gz
+    tar -xzf heminetwork_v0.4.3_linux_arm64.tar.gz > /dev/null
     cd heminetwork_v0.4.3_linux_amd64 || { show "Failed to change directory."; exit 1; }
 
 elif [ "$ARCH" == "arm64" ]; then
     show "Downloading for arm64 architecture..."
-    wget --quiet --show-progress https://github.com/hemilabs/heminetwork/releases/download/v0.4.3/heminetwork_v0.4.4_linux_amd64.tar.gz -O heminetwork_v0.4.3_linux_arm64.tar.gz
+    wget --quiet --show-progress https://github.com/hemilabs/heminetwork/releases/download/v0.4.4/heminetwork_v0.4.4_linux_arm64.tar.gz -O heminetwork_v0.4.3_linux_arm64.tar.gz
     tar -xzf heminetwork_v0.4.3_linux_arm64.tar.gz > /dev/null
     cd heminetwork_v0.4.3_linux_arm64 || { show "Failed to change directory."; exit 1; }
 
